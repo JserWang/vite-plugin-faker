ebc69659b656ca0ee9171bcbf9cff209ed584cfb
JserWang <jserwang@gmail.com>
2020-12-23 10:33:05 +0800

    chore: increase the playground main entrance for dev

---
+++ playground/main.ts
@@ -0,0 +1,10 @@
+import { MockDataResolver } from '../src/resolver/mockData';
+
+const mockDataResolver = new MockDataResolver({
+  basePath: '/playground',
+  includes: [/^.*Service/],
+  mockFile: true,
+  watchFile: true,
+});
+
+mockDataResolver.getMockData();
